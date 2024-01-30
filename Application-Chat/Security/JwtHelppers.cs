6f4f0537864ea88b7d22a29d5c80cfc950a7b778
KevinChan <al066225@uacam.mx>
2024-01-30 10:32:50 -0600

    adding settings JWT

---
+++ Application-Chat/Security/JwtHelppers.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Application_Chat.Security
+{
+	public static class JwtHelppers
+	{
+
+		public static IEnumerable<Claim> GetClaims(this UserTokens userAccounts, Guid Id)
+		{
+			List<Claim> claims = new List<Claim>
+			{
+				new Claim("Id",userAccounts.Id.ToString()),
+				new Claim(ClaimTypes.Name, userAccounts.UserName),
+				new Claim(ClaimTypes.Expiration, DateTime.Now.AddDays(1).ToString("G")),
+				new Claim(ClaimTypes.Role, userAccounts.Rol)
+			};
+
+			return claims;
+		}
+
+		public static IEnumerable<Claim> GetClaims(this UserTokens userAccounts, out Guid Id)
+		{
+			Id = Guid.NewGuid();
+
+			return GetClaims(userAccounts, Id);
+		}
+
+		public static UserTokens GenerateToken(UserTokens modeL, JwtSettings jwtSettings)
+		{
+			try
+			{
+				UserTokens userToken = new UserTokens();
+
+				if (userToken == null)
+				{
+					throw new ArgumentNullException(nameof(userToken));
+				}
+				//Obtain key secret
+				var key = System.Text.Encoding.ASCII.GetBytes(jwtSettings.IssuerSigningKey);
+
+				Guid Id;
+				//Expire
+				DateTime expireTime = DateTime.Now.AddDays(1);
+
+				//Validty
+				userToken.Validty = expireTime.TimeOfDay;
+
+				//GENERATE OUR JWT
+
+				var jwToken = new JwtSecurityToken(
+					issuer: jwtSettings.ValidIssuer,
+					audience: jwtSettings.ValidAudience,
+					claims: GetClaims(modeL, out Id),
+					notBefore: new DateTimeOffset(DateTime.Now).DateTime,
+					expires: new DateTimeOffset(expireTime).DateTime,
+					signingCredentials: new SigningCredentials
+					(
+						new SymmetricSecurityKey(key),
+						SecurityAlgorithms.HmacSha256
+					)
+					);
+				userToken.Token = new JwtSecurityTokenHandler().WriteToken(jwToken);
+				userToken.UserName = modeL.UserName;
+				userToken.Id = modeL.Id;
+				userToken.GuidId = modeL.GuidId;
+				userToken.Rol = modeL.Rol;
+
+				return userToken;
+			}
+			catch (Exception ex)
+			{
+
+				throw new Exception("Error generating thw JWT", ex);
+			}
+		}
+	}
+}
---
+++ Application-Chat/Security/JwtSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Application_Chat.Security
+{
+	public class JwtSettings
+	{
+		public bool ValidateIssuerSigningKey { get; set; }
+		public string IssuerSigningKey { get; set; }
+		public bool ValidateIssuer { get; set; } = false;
+		public string? ValidIssuer { get; set; }
+		public bool ValidateAudience { get; set; } = false;
+		public string? ValidAudience { get; set; }
+		public bool RequiredExpirationTime { get; set; } = true;
+		public bool ValidateLifetime { get; set; }
+	}
+}
