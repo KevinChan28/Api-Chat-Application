51a0d01de8c1e2517d81da682c0b5eed8fddaf9a
Kevin Adrian Chan Guzman <79293311+KevinChan28@users.noreply.github.com>
2024-02-01 22:59:52 -0600

    Refactoring group (#11)
    * error launch
    
    * refactoring Group

--- Application-Chat/DTO/CreateGroup.cs
+++ Application-Chat/DTO/CreateGroup.cs
@@ -1,11 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Application_Chat.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application_Chat.DTO
 {
-    public record CreateGroup(
-        [Required]
-        string Name,
-        string? Image,
-        string? Description
-        );
+	public record CreateGroup(
+		[Required]
+		string Name,
+		string? Image,
+		string? Description,
+		[Required]
+		VisibilityType Visibility,
+		[Required]
+		DateTime CreatedDate
+		);
 }
--- Application-Chat/Service/Imp/ImpGroup.cs
+++ Application-Chat/Service/Imp/ImpGroup.cs
@@ -1,78 +1,84 @@
 ﻿using Application_Chat.DTO;
 using Application_Chat.Models;
 using Application_Chat.Repository;
-using Application_Chat.Security;
 
 namespace Application_Chat.Service.Imp
 {
-    public class ImpGroup : IGroup
-    {
-        private IGroupRepository _groupRepository;
-        private JwtSettings _jwtSettings;
+	public class ImpGroup : IGroup
+	{
+		private IGroupRepository _groupRepository;
+		private IAuthorization _authorization;
 
-        public ImpGroup(IGroupRepository groupRepository, JwtSettings jwtSettings)
-        {
-            _groupRepository = groupRepository;
-            _jwtSettings = jwtSettings;
-        }
+		public ImpGroup(IGroupRepository groupRepository, IAuthorization authorization)
+		{
+			_groupRepository = groupRepository;
+			_authorization = authorization;
+		}
 
-        public async Task<string> Create(CreateGroup model)
-        {
-            Group group = new Group
-            {
-                Name = model.Name,
-                Image = model.Image,
-                Description = model.Description
-            };
+		public async Task<string> Create(CreateGroup model)
+		{
+			Group group = new Group
+			{
+				Name = model.Name,
+				Image = model.Image,
+				Description = model.Description,
+				CreatedDate = model.CreatedDate,
+				Visibility = model.Visibility,
+				Owner = _authorization.UserCurrent()
+			};
 
-            string idGroup = await _groupRepository.CreateGroup(group);
-            return idGroup;
-        }
+			string idGroup = await _groupRepository.CreateGroup(group);
 
-        public async Task<bool> DeleteGroup(string idGroup)
-        {
-            Group groupFind = await _groupRepository.GetGroupById(idGroup);
-            if(groupFind != null)
-            {
-                await _groupRepository.DeleteGroup(idGroup);
+			return idGroup;
+		}
 
-                return true;
-            }
+		public async Task<bool> DeleteGroup(string idGroup)
+		{
+			Group groupFind = await _groupRepository.GetGroupById(idGroup);
 
-            return false;
-        }
+			if (groupFind != null)
+			{
+				await _groupRepository.DeleteGroup(idGroup);
 
-        public async Task<List<Group>> GetAllGroups()
-        {
-            return await _groupRepository.GetAllGroups();
-        }
+				return true;
+			}
 
-        public async Task<Group> GetGroupById(string idGroup)
-        {
-            Group group = await _groupRepository.GetGroupById(idGroup);
-            if (group != null)
-            {
-                return group;
-            }
+			return false;
+		}
 
-            return null;
-        }
+		public async Task<List<Group>> GetAllGroups()
+		{
+			return await _groupRepository.GetAllGroups();
+		}
 
-        public async Task<string> UpdateGroup(ChangeGroup model)
-        {
-            Group group = await _groupRepository.GetGroupById(model.id);
-            if (group != null)
-            {
-                group.Image = model.Image;
-                group.Name = model.Name;
-                group.Description = model.Description;
-                string id = await _groupRepository.UpdateGroup(group);
+		public async Task<Group> GetGroupById(string idGroup)
+		{
+			Group group = await _groupRepository.GetGroupById(idGroup);
 
-                return id;
-            }
+			if (group != null)
+			{
+				return group;
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-    }
+		public async Task<string> UpdateGroup(ChangeGroup model)
+		{
+			Group group = await _groupRepository.GetGroupById(model.id);
+
+			if (group != null)
+			{
+				group.Image = model.Image;
+				group.Name = model.Name;
+				group.Description = model.Description;
+				string id = await _groupRepository.UpdateGroup(group);
+
+				return id;
+			}
+
+			return null;
+		}
+
+	}
 }
